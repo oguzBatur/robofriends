e2bc0cbfab0905b2f20fe6421aea0a07872445f6
oguzBatur <sariozbatur@gmail.com>
2021-09-07 15:08:41 +0300

    Robofriends site is finished!

--- src/App.js
+++ src/App.js
@@ -1,13 +1,58 @@
 import React from "react";
-import Card from "./Card";
 import CardList from "./CardList";
-import { robots } from "./robots";
+import SearchBox from "./SearchBox.js";
+import './App.css';
+import Scroll from './Scroll.js';
+ 
 
-const App = () =>{
+class App extends React.Component{
+    constructor(){
+        super();
+        this.state = {
+            robots: [],
+            searchfield: ''
+        }
+    }
 
-    return(
+    componentDidMount(){
+        fetch('https://jsonplaceholder.typicode.com/users').then(response =>{
+            return response.json();
+        }).then(user => {
+            this.setState({robots: user});
 
-        <CardList robots={robots}/>
-    )
+        })
+    }
+
+
+    OnSearchChange = (event) => {
+        this.setState({searchfield: event.target.value});
+
+    }
+    
+    render(){
+
+        const filteredRobots = this.state.robots.filter(robot => {
+            return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+        });
+
+        if (this.state.robots.length === 0) {
+            return <h1 className='tc'>Loading....</h1>
+        }
+        else{
+
+        
+            return(
+                <div className= 'tc'>
+                    <h1>RoboFriends</h1>
+                    <SearchBox searchChange = {this.OnSearchChange}/>
+                    <Scroll>
+                        <CardList robots={filteredRobots}/>
+                    </Scroll>
+                </div>
+            
+                );
+            }
+        }
 }
+
 export default App;
